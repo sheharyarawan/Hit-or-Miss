09068fdc8c36356ce447c89842f9e050b9b81a4a
Sheharyar Awan <sheharyarawan1153@gmail.com>
2026-05-02 16:52:12 +0500

    database created

---
+++ app/src/main/java/com/example/hitormiss/data/room/AppDatabase.kt
@@ -0,0 +1,41 @@
+package com.example.hitormiss.data.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.hitormiss.data.entity.Player
+import com.example.hitormiss.data.entity.PlayerStatsSummary
+
+@Database(
+    entities = [
+        Player::class,
+        PlayerStatsSummary::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun playerDao(): PlayerDao
+    abstract fun playerStatsDao(): PlayerStatsDao
+
+    companion object{
+        @Volatile
+
+        private var instance: AppDatabase?=null
+        var LOCK=Any()
+
+        operator fun invoke(context: Context)=instance?:
+        synchronized(LOCK){
+            instance?: createDB(context).also{
+                instance=it
+            }
+        }
+        private fun createDB(context: Context)= Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            name = "Cricket_db"
+        ).build()
+    }
+}
