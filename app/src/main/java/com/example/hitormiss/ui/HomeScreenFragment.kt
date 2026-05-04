5b9390330f6e89b1320b964c6d5a1998ff65a143
Sheharyar Awan <sheharyarawan1153@gmail.com>
2026-05-04 10:12:42 +0500

    Click on card views setup in fragment

--- app/src/main/java/com/example/hitormiss/ui/HomeScreenFragment.kt
+++ app/src/main/java/com/example/hitormiss/ui/HomeScreenFragment.kt
@@ -2,11 +2,65 @@ package com.example.hitormiss.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.hitormiss.R
+import com.example.hitormiss.databinding.FragmentHomeScreenBinding
+import com.example.hitormiss.utils.engine.StatCategory
 
-class HomeScreenFragment : Fragment() {
+class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
 
+    private var _binding: FragmentHomeScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentHomeScreenBinding.bind(view)
+
+        setupClicks()
+    }
+
+    private fun setupClicks() {
+
+        binding.cardViewRuns.setOnClickListener {
+            openGame(StatCategory.RUNS)
+        }
+
+        binding.cardViewSixes.setOnClickListener {
+            openGame(StatCategory.SIXES)
+        }
+
+        binding.cardViewFours.setOnClickListener {
+            openGame(StatCategory.FOURS)
+        }
+
+        binding.cardViewWickets.setOnClickListener {
+            openGame(StatCategory.WICKETS)
+        }
+
+        binding.cardViewSR.setOnClickListener {
+            openGame(StatCategory.STRIKE_RATE)
+        }
+
+        binding.cardViewEconomy.setOnClickListener {
+            openGame(StatCategory.ECONOMY)
+        }
+    }
+
+    private fun openGame(category: StatCategory) {
+
+        val bundle = Bundle().apply {
+            putString("category", category.name)
+        }
+
+        /*        findNavController().navigate(
+            R.id.action_homeFragment_to_gameFragment,
+            bundle
+        )*/
+    }
+
+    override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+    }
 }
