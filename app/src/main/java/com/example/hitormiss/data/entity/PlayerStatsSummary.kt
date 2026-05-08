d5e5044823723a914eb136b1f882904bbe549647
Sheharyar Awan <sheharyarawan1153@gmail.com>
2026-05-08 09:10:21 +0500

    format wise updated

--- app/src/main/java/com/example/hitormiss/data/entity/PlayerStatsSummary.kt
+++ app/src/main/java/com/example/hitormiss/data/entity/PlayerStatsSummary.kt
@@ -9,13 +9,39 @@ data class PlayerStatsSummary(
     @PrimaryKey val playerId: String,
 
     val runs: Int,
-    val sixes: Int,
+    val testRuns: Int,
+    val odiRuns: Int,
+    val t20iRuns: Int,
+
     val fours: Int,
+    val testFours: Int,
+    val odiFours: Int,
+    val t20iFours: Int,
+
+    val sixes: Int,
+    val testSixes: Int,
+    val odiSixes: Int,
+    val t20iSixes: Int,
+
     val strikeRate: Float,
+    val testStrikeRate: Float,
+    val odiStrikeRate: Float,
+    val t20iStrikeRate: Float,
+
     val average: Float,
+    val testAverage: Float,
+    val odiAverage: Float,
+    val t20iAverage: Float,
 
     val wickets: Int,
+    val testWickets: Int,
+    val odiWickets: Int,
+    val t20iWickets: Int,
+
     val economy: Float,
+    val testEconomy: Float,
+    val odiEconomy: Float,
+    val t20iEconomy: Float,
 
     val matches: Int
 )
