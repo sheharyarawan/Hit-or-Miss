4497fe2ca361bbb1ba83530d83f93a0bf4c2224d
Sheharyar Awan <sheharyarawan1153@gmail.com>
2026-05-08 09:08:27 +0500

    response corrected

--- app/src/main/java/com/example/hitormiss/data/api/PlayerApi.kt
+++ app/src/main/java/com/example/hitormiss/data/api/PlayerApi.kt
@@ -1,13 +1,16 @@
 package com.example.hitormiss.data.api
 
 import com.example.hitormiss.data.model.PlayerInfo
+import com.example.hitormiss.data.model.PlayerResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PlayerApi {
 
-    @GET("player/{id}")
-    suspend fun getPlayer(
-        @Path("id") id: String
-    ): PlayerInfo
+    @GET("players_info")
+    suspend fun getPlayerInfo(
+        @Query("apikey") apiKey:String ,
+        @Query("id") playerId: String
+    ): PlayerResponse
 }
