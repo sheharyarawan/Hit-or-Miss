5225d4d31b0f633e496982bcc38bf3d5c8b68056
Sheharyar Awan <sheharyarawan1153@gmail.com>
2026-05-08 09:31:13 +0500

    updated

--- app/src/main/java/com/example/hitormiss/MainActivity.kt
+++ app/src/main/java/com/example/hitormiss/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hitormiss.utils.AppDependencies
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        AppDependencies.init(this)
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
