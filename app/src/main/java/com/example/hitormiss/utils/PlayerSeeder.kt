89d934b6a4feea803a9153519109787aeca6b3e9
Sheharyar Awan <sheharyarawan1153@gmail.com>
2026-05-17 14:53:02 +0500

    more players added

--- app/src/main/java/com/example/hitormiss/utils/PlayerSeeder.kt
+++ app/src/main/java/com/example/hitormiss/utils/PlayerSeeder.kt
@@ -13,19 +13,19 @@ fun getPlayers(): List<PlayerSeed> {
         PlayerSeed("fd2de7fb-6bd7-4a66-ab3a-ca82f0e87a6a", "Chris Gayle"),
         PlayerSeed("64ab5424-779f-44ad-8135-21097f6eb83c", "Sachin Tendulkar"),
 
-       /* PlayerSeed("20696a2d-57c9-4ba9-850d-7d8f5bb88663", "Lasith Malinga"),
+        PlayerSeed("20696a2d-57c9-4ba9-850d-7d8f5bb88663", "Lasith Malinga"),
         PlayerSeed("a8dfc4ca-f586-457c-9012-8823e754e728", "Wasim Akram"),
         PlayerSeed("021f619b-a637-448f-9d4b-da7ddbdc2a03", "Glenn McGrath"),
         PlayerSeed("6602d875-cf56-46a3-866c-de80aaa006bc", "Jasprit Bumrah"),
         PlayerSeed("a53b7dc0-5e77-43d4-9e3a-409876d22064", "Shane Warne"),
-*/
+
         PlayerSeed("b6e7aa41-b0bb-4b0f-8f59-59f729283be5", "MS Dhoni"),
         PlayerSeed("77a02164-4a36-48d4-9817-6331b474f8e0", "Adam Gilchrist"),
         PlayerSeed("1ead434a-4e4a-4b3e-a70a-71f7dc7751cd", "Kumar Sangakkara"),
 
         PlayerSeed("fcea2e94-fcf3-4d97-90b8-9caa41b63e57", "Jacques Kallis"),
-      /*  PlayerSeed("abedb8ad-b806-4c99-9762-b26d8d5a00f1", "Imran Khan"),
-        PlayerSeed("fc09ce60-e7e4-44e5-9a53-67a617560161", "Ben Stokes")*/
+        PlayerSeed("abedb8ad-b806-4c99-9762-b26d8d5a00f1", "Imran Khan"),
+        PlayerSeed("fc09ce60-e7e4-44e5-9a53-67a617560161", "Ben Stokes")
 
     )
 }
